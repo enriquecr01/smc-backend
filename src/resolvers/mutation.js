cbdc985cd00994881b9f72d1e4bd924cb0a834e5
Enrique Chavez <enriquecr01@live.com>
2020-01-30 16:52:19 -0800

    Agrega estudiantes y autos

--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -1,4 +1,6 @@
 import University from './../models/University';
+import Student from './../models/Student';
+import Car from './../models/Car';
 
 export const mutation = {
     Mutation: {
@@ -6,6 +8,21 @@ export const mutation = {
             const newUniversity = new University(input);
             await newUniversity.save();
             return newUniversity;
+        },
+        async createStudent(__, { input }) {
+            const defaultValues = { raiting: 5, photo: 'default.png' };
+            input = {
+                ...input,
+                ...defaultValues
+            };
+            const newStudent = new Student(input);
+            await newStudent.save();
+            return newStudent;
+        },
+        async createCar(__, { input }) {
+            const newCar = new Car(input);
+            await newCar.save();
+            return newCar;
         }
         // async deleteUser(__, { _id }) {
         //     return await User.findByIdAndDelete(_id);
